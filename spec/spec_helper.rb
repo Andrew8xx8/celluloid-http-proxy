5e0f8ba12c4dfee57ac05ac07e316cf19e013f6a
Andrew8xx8 <avk@8xx8.ru>
2013-01-14 17:20:08 +0400

    Coveralls added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'celluloid-http-proxy'
 
+require 'coveralls'
+Coveralls.wear!
+
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
