afe09e24c4fefae0bfad6d192c9b66ad2adc3655
Andrew8xx8 <avk@8xx8.ru>
2013-01-14 14:01:21 +0400

    Request|Response named correctly now

--- lib/celluloid-http-proxy/transformer.rb
+++ lib/celluloid-http-proxy/transformer.rb
@@ -1,14 +1,10 @@
 class Celluloid::Http::Proxy::Transformer
-  autoload :Request, 'celluloid-http-proxy/proxy/request'
-  autoload :Response, 'celluloid-http-proxy/proxy/response'
-
   def initialize(request)
-    @request = Request.build_from_request request
-    extract_movie_source
+    @request = Celluloid::Http::Proxy::Request.build_from_request request
   end
 
   def transform_response_for_client(response)
-    transform_response Response.build_from_response response
+    transform_response Celluloid::Http::Proxy::Response.build_from_response response
   end
 
   def transform_request_for_backend
