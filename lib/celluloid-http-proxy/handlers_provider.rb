7706846a4c56508c878d8f9d201461d1b2a7d106
Andrew8xx8 <avk@8xx8.ru>
2013-01-14 13:51:33 +0400

    HandlersProvider now can register handlers on conditions

--- lib/celluloid-http-proxy/handlers_provider.rb
+++ lib/celluloid-http-proxy/handlers_provider.rb
@@ -14,24 +14,37 @@ class Celluloid::Http::Proxy::HandlersProvider
     instance_eval &block
   end
 
-  def handler(name, &block)
-    @handlers << {
-      name: name,
-      block: block
-    }
-  end
-
-  def get(request = nil)
-    @handlers
-  end
-
   def condition(name, &block)
     @conditions[name] = block
   end
 
-  def apply_condition(name, request)
-    raise CustomConditionNotFound unless @conditions.has_key? name
+  def on(condition, &block)
+    @handlers << {
+      condition: condition,
+      block: block
+    }
+  end
 
-    @conditions[name].call request
+  def get_condition(name)
+    raise CustomConditionNotFound unless condition_exists? name
+
+    @conditions[name]
+  end
+
+  def get_handlers(condition, request)
+    return search_handlers(condition) if apply_condition(condition, request)
+  end
+
+private
+  def search_handlers(condition)
+    @handlers.select {|s| s[:condition] == condition }
+  end
+
+  def apply_condition(name, request)
+    get_condition(name).call request
+  end
+
+  def condition_exists?(name)
+    @conditions.has_key? name
   end
 end
--- spec/celluloid-http-proxy/handlers_provider_spec.rb
+++ spec/celluloid-http-proxy/handlers_provider_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 require 'resolv'
 
 describe Celluloid::Http::Proxy::HandlersProvider do
+  it 'can raise exception on trying to get missing condition' do
+    handlers_provider = Celluloid::Http::Proxy::HandlersProvider.register do
+    end
+
+    expect { handlers_provider.get_condition(:missing_condition) }.to raise_error
+  end
+
   it 'can register custom condition' do
     handlers_provider = Celluloid::Http::Proxy::HandlersProvider.register do
       condition :custom_condition do 
@@ -9,18 +16,29 @@ describe Celluloid::Http::Proxy::HandlersProvider do
       end
     end
 
-    handlers_provider.apply_condition(:custom_condition, {}).should be true
+    handlers_provider.get_condition(:custom_condition).should be_a_kind_of Proc
   end
 
-  it 'can register custom condition with param' do
+  it 'can register handler on custom condition' do
+    request = Celluloid::Http::Proxy::Request.new( "http://localhost:8080/proxy/movies.json?movie_source_id=1" )
+
     handlers_provider = Celluloid::Http::Proxy::HandlersProvider.register do
-      condition :custom_condition do |request|
-        request.host == "localhost"
+      condition :custom_condition do |r|
+        r.host == "localhost"
+      end
+
+      on :custom_condition do
+        true
+      end
+
+      on :another_condition do
+        true
       end
     end
 
-    request = Celluloid::Http::Proxy::Request.new( "http://localhost:8080/proxy/movies.json?movie_source_id=1" )
-
-    handlers_provider.apply_condition(:custom_condition, request).should be true
+    handlers = handlers_provider.get_handlers(:custom_condition, request)
+    handlers.count.should eq 1 
+    handlers.first[:condition].should eq :custom_condition
+    handlers.first[:block].should be_a_kind_of Proc
   end
 end
